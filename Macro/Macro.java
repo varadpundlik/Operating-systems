a23d6cb43721f082b808271a377bd04dc4eb5ea5
varadpundlik <varadpundlik@gmail.com>
2023-09-22 11:33:04 +0530

    Started Macro preprocessor

---
+++ Macro/Macro.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+class MDT{
+    String macro_defn;
+
+    MDT(String md){
+        this.macro_defn=md;
+    }
+};
+
+class MNT{
+    String name;
+    int mdt_index;
+
+    MNT(String n,int idx){
+        this.name=n;
+        this.mdt_index=idx;
+    }
+};
+
+class ALA{
+    String arg;
+
+    ALA(String a){
+        this.arg=a;
+    }
+};
+
+public class Macro{
+    Vector<MNT> mnt=new Vector<MNT>();
+    Vector<MDT> mdt=new Vector<MDT>();
+    Vector<ALA> ala=new Vector<ALA>();
+
+    public static void pass1() throws Exception{
+        File input=new File("/home/student/Desktop/31467_LP1/Macro/input.txt");
+        Scanner sc=new Scanner(input);
+
+        while (sc.hasNextLine())
+            System.out.println(sc.nextLine());
+    }
+
+    public static void pass2(){
+
+    }
+
+    public static void main(String args[]){
+        System.out.println("Hello World");
+        try{
+            pass1();
+        }catch(Exception e){
+            System.out.println("File not found");
+        }
+    }
+}
